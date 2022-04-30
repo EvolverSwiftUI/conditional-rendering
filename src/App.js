a68a32f0aba39186eab3d80d87fe35c9c6dff7b2
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-30 13:42:18 +0530

    addons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import Welcome from './components/Welcome';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {isLoggedIn: true};
+
+  // renderAuthButton = () => {
+  //   const {isLoggedIn} = this.state;
+  //   if (isLoggedIn === true) {
+  //     return <button>Log In</button>
+  //   }
+  //   return <button>Log Out</button>
+  // }
+
+  render() {
+    const {isLoggedIn} = this.state;
+
+    // let authButton;
+    // if (isLoggedIn === true) {
+    //   authButton = <button>Log In</button>
+    // } else {
+    //   authButton = <button>Log Out</button>
+    // }
+    return(
+      <div>
+        <Welcome greeting="Hello" user="Sivaram"/>
+        {/* {this.renderAuthButton()}         */}
+        {/* {authButton} */}
+        {/* {isLoggedIn ? <button>Log Out</button> : <button>Log In</button>} */}
+        {isLoggedIn && <button>Log Out</button>}
+        {!isLoggedIn && <button>Log In</button>}
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
